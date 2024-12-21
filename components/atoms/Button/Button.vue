451518ae79679c6ce9d0e5a2817f6a085c469d96
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2024-12-20 23:11:25 -0300

    feat

--- components/atoms/Button/Button.vue
+++ components/atoms/Button/Button.vue
@@ -1,24 +1,52 @@
 <script setup lang="ts">
+import { computed, defineProps } from "vue";
+
+type ItemSize = "small" | "medium" | "large";
+type ButtonType = "outlined" | "contained" | "containedSecondary";
+
+const buttonSize: Record<ItemSize, string> = {
+  small: "px-6 py-3 text-xs",
+  medium: "px-8 py-3.5 text-sm",
+  large: "px-10 py-4 text-base",
+};
+
+const types: Record<ButtonType, string> = {
+  outlined:
+    "!border-2 !border-[#00B207] hover:border-[#2C742F] text-[#00B207] hover:text-[#2C742F]",
+  contained: "bg-[#00B207] text-white hover:bg-[#2C742F]",
+  containedSecondary:
+    "bg-[#EEF7ED] text-[#00B207] hover:bg-[#D5E3D5] hover:text-[#2C742F]",
+};
+
+function getButtonSize(size: ItemSize): string {
+  return buttonSize[size];
+}
+
+function getType(type: ButtonType): string {
+  return types[type];
+}
+
+// Definir valores padrão para as props
+const props = defineProps<{
+  sizeType?: ItemSize;
+  buttonType?: ButtonType;
+  text?: string;
+  onClick?: () => void;
+}>();
+
+// Atribuindo valores padrão caso não sejam passados
+const sizeType = props.sizeType || "large";
+const buttonType = props.buttonType || "contained";
+const text = props.text || "Default Text";
+
+const buttonStyle = computed(() =>
+  `${getButtonSize(sizeType)} ${getType(buttonType)} rounded-full transition duration-300 font-semibold`
+);
 
 </script>
 
 <template>
-  <button type="submit" class="button">Entrar</button>
+  <button :class="buttonStyle" @click="props.onClick">
+    {{ text }}
+  </button>
 </template>
-
-
-<style>
-  .button{
-    background-color:#468499;
-    border: none;
-    height: 38px;
-    color: white;
-    border-radius: 4px;
-    font-weight: bold;
-  }
-  
-  @media (max-width: 768px) {
-   
-  }
-</style>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,140 +1,14 @@
 <script setup lang="ts">
-import '@/assets/css/tailwind.css'
-import ListProducts from '~/components/organisms/ListProducts/ListProducts.vue';
-import Header from '~/components/molecules/Header/Header.vue';
+import "@/assets/css/tailwind.css";
+import Button from "~/components/atoms/Button/Button.vue";
+
+
+
 </script>
 
+
 <template>
-  <div class="container ">
-    <Header />
-    <ListProducts />
+  <div>
+
   </div>
 </template>
-
-<style lang="css">
-h1,
-h2,
-h3,
-h4,
-h5 {
-  padding: 0;
-  margin: 0px;
-}
-
-a {
-  text-decoration: none;
-  color: #000;
-}
-
-.items-center {
-  align-items: center;
-}
-
-body {
-  height: 100%;
-}
-
-.flex {
-  display: flex;
-}
-
-.flex-column {
-  flex-direction: column;
-}
-
-.flex-row {
-  flex-direction: row;
-}
-
-.container {
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-}
-
-.flex-col {
-  flex-direction: column;
-}
-
-
-.justify-between {
-  justify-content: space-between;
-}
-
-ul {
-  list-style-type: none;
-  margin: 0 !important;
-  padding: 0;
-}
-
-.flex-col {
-  flex-direction: column;
-}
-
-.menu-mobile {
-  display: none;
-  border: none;
-  font-size: 22px;
-  background-color: transparent;
-}
-
-.gap-2 {
-  gap: 2px;
-}
-
-.gap-4 {
-  gap: 4px;
-}
-
-.gap-8 {
-  gap: 8px;
-}
-
-.gap-16 {
-  gap: 16px;
-}
-
-.active {
-  border-bottom: 2px solid #f63e4e;
-}
-
-.mobile-container {
-  display: none;
-}
-
-@media (max-width: 768px) {
-  .product-list ul {
-    grid-template-columns: repeat(2,
-        1fr);
-    /* Altera para 2 colunas em telas menores */
-  }
-
-  .product-list {
-    margin-top: 40px;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .menu-navigation {
-    display: none;
-  }
-
-  .menu-mobile {
-    display: flex;
-  }
-
-  .mobile-container {
-    display: flex;
-  }
-}
-
-@media (max-width: 480px) {
-  .product-list ul {
-    grid-template-columns: 1fr;
-  }
-}
-
-.justify-center {
-  justify-content: center;
-}
-</style>
